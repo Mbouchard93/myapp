b56874561678b572794546d1f997f579c5819906
Mbouchard93 <mariiane.bouchard12@gmail.com>
2024-10-18 15:09:47 -0400

    todo

---
+++ src/compoments/todo.svelte
@@ -0,0 +1,40 @@
+<script>
+    let todos = [
+        {
+            name : 'tache',
+            done : false
+        }
+    ]
+
+    let newtodo = ''
+
+    function addtodo() {
+        todos = [...todos, {
+            name : newtodo,
+            done : false
+        }]
+    
+        newtodo = ''
+    }
+
+</script>
+
+<h1>Todo Liste</h1>
+
+<ul>
+    {#each todos as todo}
+    <li>
+        <input type="checkbox" bind:checked={todo.done}>
+         <span class:done="{todo.done}" >{todo.name}</span>
+        </li>
+    {/each}
+</ul>
+
+<input type="text" bind:value={newtodo}>
+<button on:click={addtodo} >ajouter</button>
+
+<style>
+    .done{
+        text-decoration: line-through;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    import Todo from "../compoments/todo.svelte";
+</script>
+
+<Todo/>
